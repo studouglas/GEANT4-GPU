b0562db7583d61f5ef6c5a4f390fdb5d6748884f
root <root@localhost.localdomain>
2015-11-10 14:13:44 +0000

    temp file exception in git ignore -gorrierw

---
+++ geant4.10.00.p02/examples/basic/B1-build/CMakeFiles/B1.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/B1"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/B1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
