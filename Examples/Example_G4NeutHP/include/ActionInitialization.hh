06276b563e5ab722fb3bdc70dbb9c0de68c67510
GorrieXIV <rob-gxiv@hotmail.com>
2016-01-19 20:39:57 -0500

    G4NeutHP updates -gxiv

---
+++ Examples/Example_G4NeutHP/include/ActionInitialization.hh
@@ -0,0 +1,8 @@
+#ifndef ActionInitialization_h
+#define ActionInitialization_h 1
+
+#include "G4VUserActionInitialization.hh"
+
+
+#endif
+
---
+++ Examples/Example_G4NeutHP/include/DetectorConstruction.hh
@@ -0,0 +1,17 @@
+#include "DetectorConstruction.hh"
+#include "DetectorMessenger.hh"
+#include "G4Material.hh"
+#include "G4NistManager.hh"
+
+#include "G4Box.hh"
+#include "G4LogicalVolume.hh"
+#include "G4PVPlacement.hh"
+
+#include "G4GeometryManager.hh"
+#include "G4PhysicalVolumeStore.hh"
+#include "G4LogicalVolumeStore.hh"
+#include "G4SolidStore.hh"
+#include "G4RunManager.hh"
+
+#include "G4UnitsTable.hh"
+#include "G4SystemOfUnits.hh"
