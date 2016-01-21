a86b30c3c5088355250a4b26e0d76f6c6fed7423
GorrieXIV <rob-gxiv@hotmail.com>
2016-01-21 12:11:10 +0000

    G4NeutHP -gxiv

--- Examples/Example_G4NeutHP/include/ActionInitialization.hh
+++ Examples/Example_G4NeutHP/include/ActionInitialization.hh
@@ -3,6 +3,24 @@
 
 #include "G4VUserActionInitialization.hh"
 
+class DetectorConstruction;
+class G4VSteppingVerbose;
+
+class ActionInitialization : public G4VUserActionInitialization{
+	public:
+
+    ActionInitialization(DetectorConstruction* detector);
+		virtual ~ActionInitialization();
+
+		virtual void BuildForMaster() const;
+		virtual void Build() const;
+    
+		virtual G4VSteppingVerbose* InitializeSteppingVerbose() const;
+   
+	private:
+
+		DetectorConstruction* fDetector;
+};
 
 #endif
 
