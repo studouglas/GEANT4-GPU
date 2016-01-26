d40d8049037565bdc11c7bd463a1d01bd422a6fa
GorrieXIV <rob-gxiv@hotmail.com>
2016-01-26 14:48:39 +0000

    more example updates -gxiv

--- Examples/Example_G4NeutHP/src/DetectorConstruction.cc
+++ Examples/Example_G4NeutHP/src/DetectorConstruction.cc
@@ -21,7 +21,7 @@ DetectorConstruction::DetectorConstruction()
 	 fDetectorMessenger(0){ //initialization list for DetectorConstruction constructor
 	fBoxSide = 1*m;
 	DefineMaterials();
-	SetMaterial("Water_ts");
+	//SetMaterial("Uranium");
 	fDetectorMessenger = new DetectorMessenger(this);
 }
 
@@ -30,7 +30,9 @@ DetectorConstruction::~DetectorConstruction(){
 }
 
 void DetectorConstruction::DefineMaterials(){
-
+	//define uranium element
+	//create uranium material using said element (URN->AddElement(uranium)
+	//
 }
 
 G4Material* DetectorConstruction::MaterialWithSingleIsotope(G4String name, G4String symbol, G4double density, G4int Z, G4int A){ //define a material from an isoptope
@@ -38,11 +40,28 @@ G4Material* DetectorConstruction::MaterialWithSingleIsotope(G4String name, G4Str
 }
 
 G4VPhysicalVolume* DetectorConstruction::ConstructVolumes(){
-
+	//cleanup old geometry
+	G4GeometryManager::GetInstance()->OpenGeometry();
+	G4PhysicalVolumeStore::GetInstance()->Clean();
+	G4LogicalVolumeStore::GetInstance()->Clean();
+	G4SolidStore::GetInstance()->Clean();
 }
 
 void DetectorConstruction::SetMaterial(G4String materialChoice){
-
+	// search the material by its name
+  G4Material* pttoMaterial =
+     G4NistManager::Instance()->FindOrBuildMaterial(materialChoice);   
+  
+  if (pttoMaterial) { 
+    if(fMaterial != pttoMaterial) {
+      fMaterial = pttoMaterial;
+      if(fLBox) { fLBox->SetMaterial(pttoMaterial); }
+      G4RunManager::GetRunManager()->PhysicsHasBeenModified();
+    }
+  } else {
+    G4cout << "\n--> warning from DetectorConstruction::SetMaterial : "
+           << materialChoice << " not found" << G4endl;
+  } 
 }
 
 void DetectorConstruction::SetSize(G4double value){
